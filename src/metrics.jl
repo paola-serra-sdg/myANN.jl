64cc6063a1fb4ad0c0c63ed2bb8ccc953056d5b6
martina-garavaglia-sdg <martina.garavaglia@sdggroup.com>
2022-11-29 09:33:06 +0100

    accuracy

--- src/metrics.jl
+++ src/metrics.jl
@@ -3,13 +3,8 @@ using Flux: onecold
 # Accuracy
 
 function accuracy(y_true::Any, y_pred::Any)
-    s = 0
     a = onecold(y_true, 0:(size(y_true)[1]-1))
     b = onecold(y_pred, 0:(size(y_pred)[1]-1))
-    for i in range(1, size(a)[1])
-        if a[i] == b[i]
-            s = s+1
-        end
-    end
-    return s/(size(a)[1])
+    l = size(a)[1]
+    return sum(a .== b) / l
 end
